0f935fbfdbeca1740ab208f864289b6d50c28e5e
Brenda <abrendakhenya@gmail.com>
2022-12-08 04:21:49 -0800

    changing activity, "Tablas" & "Gastos", Brenda

--- app/src/main/java/com/example/overflow/MainActivity.kt
+++ app/src/main/java/com/example/overflow/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.overflow
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.overflow.databinding.ActivityMainBinding
@@ -12,5 +13,17 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        // cambiar a la cativity tablas
+        binding.buttonTablas.setOnClickListener {
+            val intent = Intent(this, Tablas_activity::class.java)
+            startActivity(intent)
+        }
+
+        // cambiar a la cativity gastos
+        binding.buttonGastos.setOnClickListener {
+            val intent = Intent(this, Gastos_activity::class.java)
+            startActivity(intent)
+        }
     }
+
 }
