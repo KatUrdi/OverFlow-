032a75e9be245acc7d11fa06385f62744bc7cc49
Brenda <abrendakhenya@gmail.com>
2022-12-08 04:09:09 -0800

    var binding, Brenda

--- app/src/main/java/com/example/overflow/MainActivity.kt
+++ app/src/main/java/com/example/overflow/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.overflow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.overflow.databinding.ActivityMainBinding
+
+private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
