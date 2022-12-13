888b54018b9048973d20f7981719c15bbe1f1bb3
Brenda <abrendakhenya@gmail.com>
2022-12-13 04:25:33 -0800

    item "gastos", Brenda

---
+++ app/src/main/java/com/example/overflow/adapter/PresentationCardAdapter.kt
@@ -0,0 +1,48 @@
+package com.example.overflow.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.example.overflow.R
+import com.example.overflow.databinding.ItemGastosCardBinding
+
+class PresentationCardAdapter :
+    RecyclerView.Adapter<PresentationCardAdapter.PresentationCardViewHolder>() {
+
+    private val presentationCard = mutableListOf<String>()
+    private var context: Context? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PresentationCardViewHolder {
+        context = parent.context
+        return PresentationCardViewHolder(
+            ItemGastosCardBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+
+    override fun onBindViewHolder(holder: PresentationCardViewHolder, position: Int) {
+        holder.binding(presentationCard[position])
+    }
+
+    override fun getItemCount(): Int = presentationCard.size
+
+    inner class PresentationCardViewHolder(private val binding: ItemGastosCardBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun binding(data: String) {/*
+            binding.textViewName.text = data
+            binding.imageProfile.setImageResource(R.drawable.vamoacalmarno)
+            binding.textViewHobie.text = context?.getText(R.string.hobie)*/
+        }
+    }
+
+    fun addPresentationCards(list: List<String>) {
+        presentationCard.clear()
+        presentationCard.addAll(list)
+    }
+}
