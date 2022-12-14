002afc665dacd09da34c7e898e71bb579e039f83
Katzumi <katurdininea02@gmail.com>
2022-12-13 22:58:58 -0400

    Commit ingresos, compra

--- app/src/main/java/com/example/overflow/Compra_activity.kt
+++ app/src/main/java/com/example/overflow/Compra_activity.kt
@@ -1,8 +1,35 @@
 package com.example.overflow
-
-import android.content.Intent
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.overflow.databinding.ActivityCompraBinding
 
-private lateinit var binding: ActivityCompraBinding
+class Compra_activity : AppCompatActivity() {
+    private lateinit var binding: ActivityCompraBinding
+    private lateinit var bindingPopup: ActivityCompraBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityCompraBinding.inflate(layoutInflater)
+        binding.buttonPopup.setOnClickListener {
+            managePopup()
+        }
+    }
+
+    fun managePopup() {
+        bindingPopup = ActivityCompraBinding.inflate(layoutInflater)
+        val dialog = Dialog(this)
+        dialog.setContentView(bindingPopup.root)
+        dialog.setCancelable(true)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        bindingPopup.textTitle.text = "Compra Añadida exitosamente"
+
+        dialog.show()
+        bindingPopup.buttonPopup.setOnClickListener {
+            dialog.dismiss()
+        }
+    }
+}
--- app/src/main/java/com/example/overflow/Ingreso_activity.kt
+++ app/src/main/java/com/example/overflow/Ingreso_activity.kt
@@ -1,8 +1,41 @@
 package com.example.overflow
 
-import android.content.Intent
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.overflow.databinding.ActivityMainBinding
 import com.example.overflow.databinding.ActivityIngresoBinding
 
-private lateinit var binding: ActivityIngresoBinding
+class Ingreso_activity : AppCompatActivity() {
+    private lateinit var binding: ActivityIngresoBinding
+
+    private lateinit var bindingPopup: ActivityIngresoBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityIngresoBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        binding.buttonPopup.setOnClickListener {
+            managePopup()
+
+        }
+    }
+
+    fun managePopup() {
+        bindingPopup = ActivityIngresoBinding.inflate(layoutInflater)
+        val dialog = Dialog(this)
+        dialog.setContentView(bindingPopup.root)
+        dialog.setCancelable(true)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        bindingPopup.textTitle.text = "Ingreso añadido exitosamente"
+
+        dialog.show()
+        bindingPopup.buttonPopup.setOnClickListener {
+            dialog.dismiss()
+        }
+    }
+}
