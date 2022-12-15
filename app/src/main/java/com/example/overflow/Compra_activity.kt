fd993e14e9a39cffb338aa965a58d458708c75ad
Katzumi <katurdininea02@gmail.com>
2022-12-15 00:42:03 -0400

    Commit general

--- app/src/main/java/com/example/overflow/Compra_activity.kt
+++ app/src/main/java/com/example/overflow/Compra_activity.kt
@@ -1,8 +1,75 @@
 package com.example.overflow
-
+import android.R
+import android.app.Dialog
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import com.example.overflow.databinding.ActivityCompraBinding
 
-private lateinit var binding: ActivityCompraBinding
+
+class Compra_activity : AppCompatActivity() {
+    private lateinit var binding: ActivityCompraBinding
+
+    private lateinit var bindingPopup: ActivityCompraBinding
+    var spinnerSelected = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityCompraBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        binding.buttonPopup.setOnClickListener {
+            managePopup()
+        }
+        initSpinner()
+    }
+
+    fun managePopup() {
+
+        bindingPopup = ActivityCompraBinding.inflate(layoutInflater)
+        val dialog = Dialog(this)
+        Toast.makeText(this@Compra_activity, "Compra añadida exitosamente!", Toast.LENGTH_SHORT)
+            .show()
+        binding.buttonPopup.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+        bindingPopup.buttonPopup.setOnClickListener {
+            dialog.dismiss()
+        }
+
+    }
+
+    fun initSpinner() {
+        val dataList = mutableListOf<String>()
+        dataList.add("Comida")
+        dataList.add("Ropa")
+        dataList.add("Alquiler")
+        dataList.add("Otros")
+
+        val adapter = ArrayAdapter(
+            this,
+            R.layout.simple_spinner_item,
+            dataList
+        )
+
+        binding.tipoCompra.adapter = adapter
+        binding.tipoCompra.onItemSelectedListener = object :
+            AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, position: Int, p3: Long) {
+                binding.textViewSelectedSpinner.text = dataList[position]
+                spinnerSelected = dataList[position]
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+            }
+
+        }
+    }
+}
--- app/src/main/java/com/example/overflow/Ingreso_activity.kt
+++ app/src/main/java/com/example/overflow/Ingreso_activity.kt
@@ -1,8 +1,52 @@
 package com.example.overflow
 
+import android.app.Dialog
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.overflow.databinding.ActivityCompraBinding
+import com.example.overflow.databinding.ActivityMainBinding
 import com.example.overflow.databinding.ActivityIngresoBinding
 
-private lateinit var binding: ActivityIngresoBinding
+class Ingreso_activity : AppCompatActivity() {
+    private lateinit var binding: ActivityIngresoBinding
+
+    private lateinit var bindingPopup: ActivityIngresoBinding
+    companion object{
+        val monto:Int= 15
+        val fecha:String="12/12/2022"
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityIngresoBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.buttonPopup.setOnClickListener {
+            managePopup()
+
+        }
+    }
+    fun managePopup() {
+        bindingPopup = ActivityIngresoBinding.inflate(layoutInflater)
+        val dialog = Dialog(this)
+        Toast.makeText(this@Ingreso_activity, "Ingreso añadido exitosamente!", Toast.LENGTH_SHORT)
+            .show()
+        binding.buttonPopup.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+        bindingPopup.buttonPopup.setOnClickListener {
+            dialog.dismiss()
+        }
+
+    }
+
+
+
+}
