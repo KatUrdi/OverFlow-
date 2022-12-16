92fb189b90bf96b43c0a851fe679de9ccbd17b08
Brenda <abrendakhenya@gmail.com>
2022-12-15 21:53:53 -0800

    "SharedPreferences", Brenda

--- app/src/main/java/com/example/overflow/Compra_activity.kt
+++ app/src/main/java/com/example/overflow/Compra_activity.kt
@@ -1,6 +1,7 @@
 package com.example.overflow
 import android.R
 import android.app.Dialog
+import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.graphics.Color
@@ -14,13 +15,20 @@ import android.widget.ArrayAdapter
 import android.widget.Toast
 import com.example.overflow.databinding.ActivityCompraBinding
 import com.example.overflow.databinding.ActivityIngresoBinding
+interface OnItemSelectedListener
 
 class Compra_activity : AppCompatActivity() {
+    companion object {
+        var MONTO: String = "56"
+        var FECHA: String = "12/12/2022"
+        var ID:String = "id"
+    }
+
     private lateinit var binding: ActivityCompraBinding
     private lateinit var bindingPopup: ActivityCompraBinding
     var spinnerSelected = ""
     private lateinit var preference: SharedPreferences
-
+    private lateinit var preference2: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,8 +36,8 @@ class Compra_activity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
         binding.buttonPopup.setOnClickListener {
-            managePopup()
-            managePreferences()
+            validDate(binding.fechaCompra.text.toString())
+
         }
         initSpinner()
     }
@@ -85,25 +93,33 @@ class Compra_activity : AppCompatActivity() {
             if (/*day*/(date[0] == '1' || date[0] == '2' || date[0] == '3' || date[0] == '0') && (date[1] == '1' || date[1] == '2' || date[1] == '3' || date[1] == '4' || date[1] == '5' || date[1] == '6' || date[1] == '7' || date[1] == '8' || date[1] == '9' || date[1] == '0')
                 /* - or / */ && (date[2] == '/' || date[2] == '-')
                 /*moth*/ && (date[3] == '1' || date[3] == '0') && (date[4] == '1' || date[4] == '2' || date[4] == '3' || date[4] == '4' || date[4] == '5' || date[4] == '6' || date[4] == '7' || date[4] == '8' || date[4] == '9' || date[4] == '0')
-                /* - or / */ && (date[5] == '/' || date[5] == '-'))
+                /* - or / */ && (date[5] == '/' || date[5] == '-')) {
                 managePopup()
-            else
+                managePreferences()
+                pasarDatos()
+            }else
                 managePopup2()
         }
     }
 
     fun managePreferences(){
         val fecha = "@id_fecha"
+        val monto = "Monto_id"
         preference = PreferenceManager.getDefaultSharedPreferences(this)
+        preference2 = PreferenceManager.getDefaultSharedPreferences(this)
         binding.buttonPopup.setOnClickListener {
             val editor = preference.edit()
             var savedData = binding.fechaCompra.text.toString()
             editor.putString(fecha, savedData)
             editor.apply()
+
+            val editor2 = preference2.edit()
+            var savedData2 = binding.montoCompra.text.toString()
+            editor.putString(monto, savedData2)
+            editor.apply()
         }
-        binding.buttonMostrar.setOnClickListener {
-            val data = preference.getString(fecha,"no hay nada ")
-            binding.textVieMostrar.text = "$data"
-        }
+    }
+    fun pasarDatos(){
+        // putExtraData(FECHA,binding.fechaCompra.text.toString())
     }
 }
